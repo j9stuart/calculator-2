9d21cff3ce39dfb617e730f841a4d9e5bd2573e7
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 18:07:32 -0700

    Made new function for unlimited arguments in addition

---
+++ arithmetic.py
@@ -0,0 +1,36 @@
+def add(*nums):
+	"""Returns the sum of the inputs"""
+	result = 0
+
+	for n in nums:
+		result += n
+
+	return result
+
+def subtract(num1, num2):
+    """Returns the result of subtracting the second number from the first"""
+    return num1 - num2
+
+def multiply(num1, num2):
+	"""Returns the product of the inputs"""
+	return num1 * num2
+
+def divide(num1, num2):
+	"""Divides the first input by the second, returning a floating point number"""
+	return float(num1) / num2
+
+def square(num1):
+	"""Returns the square of the input"""
+	return num1 ** 2
+
+def cube(num1):
+	"""Returns the cube of the input"""
+	return num1 ** 3
+
+def power(num1, num2):
+	"""Returns the result of raising the first input to the power of the second input"""
+	return num1 ** num2
+
+def mod(num1, num2):
+	"""Returns the remainder when the first input is divided by the second input"""
+	return num1 % num2
