23e760c3d716b12187d642db416857cfee3a1f91
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 18:01:13 -0700

    Created new function to take unlimited args and corresponding loop for addition

--- calculator.py
+++ calculator.py
@@ -16,35 +16,43 @@ while True:
 	if token[0] == "q": 
 		break
 	else:
-		if len(token) == 3:
-			num1 = int(token[1])
-			num2 = int(token[2])
 
-		else:
-			num1 = int(token[1])
-		
 		if token[0] == "+":
-			print add(num1, num2)
+			
+			nums=[]
+			for item in token[1:]: 
+				nums.append(int(item))
+		
+			print add(*nums)
 
 		elif token[0] == "-":
+			
 			print subtract(num1, num2)
 
 		elif token[0] == "*":
 			print multiply(num1, num2)
 
 		elif token[0] == "/":
+			num1 = int(token[1])
+			num2 = int(token[2])
 			print divide(num1, num2)
 
 		elif token[0] == "square":
+			num1 = int(token[1])
 			print square(num1)
 
 		elif token[0] == "cube":
+			num1 = int(token[1])
 			print cube(num1)
 
 		elif token[0] == "pow":
+			num1 = int(token[1])
+			num2 = int(token[2])
 			print power(num1, num2)
 
 		elif token[0] == "mod":
+			num1 = int(token[1])
+			num2 = int(token[2])
 			print mod(num1, num2)
 
 		else: 
